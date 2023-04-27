2b5dd113773089cc7838e514deaaf5665f13c574
Matt Simerson <matt@tnpi.net>
2023-04-27 13:43:35 -0700

    include testdata

--- test/fixtures/linux/setup.sh
+++ test/fixtures/linux/setup.sh
@@ -9,11 +9,9 @@ fi
 sudo killall slapd
 sudo aa-complain /usr/sbin/slapd
 
-sed -i -e '/^server/ s/:389/:3389/' -e '/^server/ s/:636$/:3636/' config/ldap.ini
-
 if [ ! -d "/tmp/slapd" ]; then sudo mkdir /tmp/slapd; fi
 sudo rm -rf /tmp/slapd/* /var/lib/ldap/*
 
 sudo slapd -f test/fixtures/linux/slapd.conf -h "ldap://localhost:3389 ldaps://localhost:3636"
 sleep 2
-ldapadd -x -D "cn=admin,dc=example,dc=com" -w "rAR84,NZ=F" -H ldap://localhost:3389 -f test/env/testdata.ldif
+ldapadd -x -D "cn=admin,dc=example,dc=com" -w "rAR84,NZ=F" -H ldap://localhost:3389 -f test/fixtures/testdata.ldif
